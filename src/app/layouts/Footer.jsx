aca0d6d774c557f215658f495bb05cae45018b3f
kaixsean <kxakai@gmail.com>
2024-03-21 09:09:56 +0000

    feat: add link to footer

--- src/app/layouts/Footer.jsx
+++ src/app/layouts/Footer.jsx
@@ -1,6 +1,17 @@
+import Link from 'next/link'
+
 function Footer() {
   return (
     <footer className="pt-5 pb-10 container mx-auto max-w-5xl">
+      <p className="text-center text-customBrown-300 dark:text-customYellow-500 font-light">
+        <Link
+          href="https://www.behance.net/gallery/144641409/Personal-Portfolio-Web-Design"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Inspire By Maham S.
+        </Link>
+      </p>
       <p className="text-center text-customBrown-300 dark:text-customYellow-500 font-light">
         © 2024. All rights reserved.
       </p>
