16517a3aa96041b9842f840c93637351c7f233d5
kaixsean <kxakai@gmail.com>
2024-03-29 06:53:42 +0000

    feat: install nanoid

--- src/app/components/Skill.jsx
+++ src/app/components/Skill.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { nanoid } from 'nanoid'
 
 function Skill(props) {
   return (
@@ -6,6 +7,7 @@ function Skill(props) {
       <div className="place-self-center">
         <div className="flex flex-col space-y-4 justify-center text-center">
           <Image
+            id={`${nanoid()}`}
             className="m-auto"
             src={`/skills/${props.src}`}
             alt={props.name}
