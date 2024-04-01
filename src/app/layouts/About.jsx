d73db7f2aed6df43c8c006db17b3fc36cbbb8f6c
kaixsean <kxakai@gmail.com>
2024-04-01 07:12:21 +0000

    feat: update about content

--- src/app/layouts/About.jsx
+++ src/app/layouts/About.jsx
@@ -4,8 +4,26 @@ function About() {
       <h6 id="about" className="text-3xl md:text-4xl py-2 font-medium">
         About
       </h6>
-      <div className="p-2 text-md md:text-xl">
-        I&apos;m a full-stack web developer with 10 years of experience
+      <div className="flex flex-row">
+        <div className="basis-1/4 mt-5 border-t-2 border-customBrown-300 dark:border-y-customYellow-500"></div>
+        <div className="p-2 text-md md:text-xl">
+          <p className="leading-relaxed">
+            I&apos;m a full-stack web developer with 10 years of experience, 2
+            years of which have been focused on API development.I also have 6
+            months of experience in cross-platform mobile app development using
+            Flutter.
+          </p>
+          <p className="leading-relaxed">
+            In my previous roles, I was responsible for the development and
+            maintenance of multiple projects, include forum and marketplace. I
+            also have experience in design mobile web and developing
+            subscription services.
+          </p>
+          <p className="leading-relaxed">
+            I have demonstrated the ability to work independently and solve
+            problems, as well as collaborate with internal and external teams.
+          </p>
+        </div>
       </div>
     </div>
   )
