478059d721fbd37d17641f90f3bc1143fc1db2bd
kaixsean <kxakai@gmail.com>
2024-07-13 03:12:23 +0000

    feat: modify security headers - X-DNS-Prefetch-Control - Strict-Transport-Security - Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: 'default-src \'self\' kaixsean.com *.kaixsean.com; script-src:https://*.googletagmanager.com; img-src:*; connect-src:https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com'
+  }
+]
+
 const nextConfig = {
   webpack(config) {
     config.module.rules.push({
@@ -6,6 +21,15 @@ const nextConfig = {
       use: ['@svgr/webpack'],
     })
     return config
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
