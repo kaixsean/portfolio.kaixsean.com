db311dd20e7a911b8b6dd663097f40a7d5c1c47c
kaixsean <kxakai@gmail.com>
2024-12-29 14:21:36 +0000

    feat: remove experiences and add projects

---
+++ src/app/layouts/Projects.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+function Projects() {
+  return (
+    <div className='py-2'>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
+        <div className="border border-0 rounded-lg border-2 bg-customBrown-500">
+          <Link
+            href="https://www.ucunik.com/"
+            target="_blank"
+            className="flex flex-col"
+          >
+            <Image
+              className="rounded-t-lg -mt-0.5 object-cover object-center md:h-52 lg:h-72"
+              src="/static/images/ucunik.png"
+              width={500}
+              height={500}
+              alt="UC UniK"
+            />
+            <h3 className="my-2 text-center">
+              UC UniK Official Website
+            </h3>
+          </Link>
+        </div>
+        <div className="border border-0 rounded-lg border-2 bg-customBrown-500">
+          <Link href="https://play.google.com/store/apps/details?id=com.whale.WhaleFinder.app&hl=zh_TW" target="_blank">
+            <Image
+              className="rounded-t-lg -mt-0.5 object-cover object-center md:h-52 lg:h-72"
+              src="/static/images/whalefinder.png"
+              width={500}
+              height={500}
+              alt="WhaleFinder"
+            />
+            <h3 className="my-2 text-center">WhaleFinder (Control Panel)</h3>
+          </Link>
+        </div>
+        <div className="border border-0 rounded-lg border-2 bg-customBrown-500">
+          <Link href="https://www.kaixsean.com" target="_blank">
+            <Image
+              className="rounded-t-lg -mt-0.5 object-cover object-center md:h-52 lg:h-72"
+              src="/static/images/myblog.png"
+              width={500}
+              height={500}
+              alt="kaix blog"
+            />
+            <h3 className="my-2 text-center">My Blog</h3>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
